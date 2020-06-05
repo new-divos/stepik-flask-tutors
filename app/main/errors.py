486753b25d7794a97617bf5f13e8854201b2af46
Roman Voronkin <roman.voronkin@gmail.com>
2020-06-05 20:37:37 +0300

    Реализованы страницы обработки ошибок

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,1 +1,17 @@
+from flask import render_template
+
 from . import main
+
+
+# noinspection PyUnusedLocal
+@main.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html',
+                           message="404. Страница не найдена"), 404
+
+
+# noinspection PyUnusedLocal
+@main.errorhandler(500)
+def internal_server_error(e):
+    return render_template('error.html',
+                           message="500. Внутренняя ошибка сервера"), 500
